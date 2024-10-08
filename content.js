acbb749ea9bbb99d5a959dc933c4b8a9ced2364d
Makoto OOSUGI <oosugi20@gmail.com>
2024-10-08 14:31:16 +0900

    軽微なリファクタとコメント追加

--- content.js
+++ content.js
@@ -1,5 +1,4 @@
 (function() {
-  console.log('memex-items-dataの取得を開始');
   const itemsDataEl = document.getElementById('memex-items-data');
   const columnsDataEl = document.getElementById('memex-columns-data');
 
@@ -34,24 +33,36 @@
   const rowGroups = document.querySelectorAll('[role="rowgroup"]');
   console.log('rowgroup elements:', rowGroups);
 
-  rowGroups.forEach(rowGroup => {
-    updateAdjustedPoints(rowGroup, groupData, neededItemsData);
-  });
-
   // project-view::rd:の要素を監視して変更があれば再描画
   const projectViewEl = document.querySelector('[id^="project-view::rd:"]');
+  //const tableViewEl = document.querySelector('[data-testid="table-root"]');
   if (projectViewEl) {
     const observer = new MutationObserver(() => {
+      // Table Viewでなければ抜ける
+      if (!projectViewEl.querySelector('[data-testid="table-root"]')) return;
+
       console.log('project-view::rd: の変更を検出しました。Adjusted Pointsを再描画します。');
-      rowGroups.forEach(rowGroup => {
-        updateAdjustedPoints(rowGroup, groupData, neededItemsData);
-      });
+      // FIXME
+      // Viewを切り替えた際に、変更は検知できているが、結果が挿入されていない。
+      // タイミングの問題な気がするがそうでもなさそう
+      // DOMの処理の仕方的に、adjustedTotalPointsElが残ったままになってるけど、
+      // なんかが変わってるから表示はされてないみたいなことっぽい？
+      // table-rootを見てると、Viewを切り替えた後は犬種自体されない
+      update();
     });
 
     observer.observe(projectViewEl, { childList: true, subtree: true });
   }
 
-  function updateAdjustedPoints(rowGroup, groupData, neededItemsData) {
+  update();
+
+  function update() {
+    rowGroups.forEach(rowGroup => {
+      renderAdjustedPoints(rowGroup, groupData, neededItemsData);
+    });
+  }
+
+  function renderAdjustedPoints(rowGroup, groupData, neededItemsData) {
     const groupName = rowGroup.getAttribute('data-testid')?.replace('table-group-', '');
     const group = groupData.find(g => g.name === groupName);
 
